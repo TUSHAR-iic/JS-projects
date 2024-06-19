0aee049f2d0c72ac553de31a5bbc8fa01f716086
TUSHAR-iic <tushar.2023@iic.ac.in>
2024-06-19 12:12:11 +0530

    second commit of the day

---
+++ project1/script.js
@@ -0,0 +1,12 @@
+// alert('hello ji')
+let number = document.getElementById('num');
+let count = 0;
+setInterval(() =>{
+    if (count == 65){
+        clearInterval();
+    }
+    else{
+    count ++;
+    number.innerHTML = count + "%";
+    }
+},30);
