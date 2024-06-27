052b838ea60a55fa7ce60391546265875f27140b
TUSHAR-iic <tushar.2023@iic.ac.in>
2024-06-27 13:43:50 +0530

    commiting files again

---
+++ smallProjects/passwordChecker/script.js
@@ -0,0 +1,11 @@
+var textBox = document.getElementById("textBox");
+var pass = "wordpress";
+var button =  document.getElementById("enter");
+button.addEventListener("click",function(){
+    while( textBox.value != pass){
+        if (textBox.value != pass) 
+          alert("wrong");
+        else
+  }
+  
+});
