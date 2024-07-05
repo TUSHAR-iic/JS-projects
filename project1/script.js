5e8f6cd892ba0ff0c3c377709081cdfe353379b3
TUSHAR-iic <tushar.2023@iic.ac.in>
2024-07-05 22:47:41 +0530

    changes have done

--- project1/script.js
+++ project1/script.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 // alert('hello ji')
 let number = document.getElementById('num');
 let count = 0;
@@ -10,17 +10,5 @@ setInterval(() =>{
     count ++;
     number.innerHTML = count + "%";
     }
-=======
-// alert('hello ji')
-let number = document.getElementById('num');
-let count = 0;
-setInterval(() =>{
-    if (count == 65){
-        clearInterval();
-    }
-    else{
-    count ++;
-    number.innerHTML = count + "%";
-    }
->>>>>>> origin/master
 },30);
+
